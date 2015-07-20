073325c338f53bec7b2cbf939f25163786888ba3
bgamwell <bgamwell@gmail.com>
2015-07-20 13:44:02 -0700

    Preparing for initial heroku deployment

--- Public/scripts/script.js
+++ Public/scripts/script.js
@@ -20,6 +20,7 @@ $(document).ready( function(){
     $signupModal.modal('show');
   });
 
+  // *****Simulated Database*****
   var userProfile = [ {
     firstname: "Brennan",
     lastname: "Gamwell",
@@ -34,10 +35,25 @@ $(document).ready( function(){
 
   // underscore templating to render userProfile to the profile page
 
-  _.each(userProfile, function (user) {
-    console.log(user);
-  var $user = $(profileTemplate(user));
-  $userInformation.append($user);
-  });
+  $.get('/api/users/current', function(user) {
+      console.log(user);
 
-});
+      // require that a user enter a first name to sign up
+
+      // pass user through underscore template
+      var $user = $(profileTemplate(user));
+      // append user to the page
+      $userInformation.append($user);
+    });
+
+
+    // _.each(userProfile, function (user) {
+    //   console.log(user);
+    // var $user = $(profileTemplate(user));
+    // $userInformation.append($user);
+    // });
+
+    var logTemplate = _.template($('#log-template').html());
+
+
+  }); // close $(document).ready
--- server.js
+++ server.js
@@ -11,6 +11,12 @@ var express = require('express'),
 // connect to mongodb
 mongoose.connect('mongodb://localhost/daily-diary');
 
+mongoose.connect(
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/daily-diary' // plug in the db name you've been using
+);
+
 // set view engine for server-side templating
 app.set('view engine', 'ejs');
 
@@ -36,6 +42,7 @@ app.use('/', function (req, res, next) {
   // finds user currently logged in based on `session.userId`
   req.currentUser = function (callback) { // "currentUser" is defined in the express middleware to manage sessions
     User.findOne({_id: req.session.userId}, function (err, user) {
+      console.log(user);
       req.user = user;
       callback(null, user);
     });
@@ -72,15 +79,14 @@ app.post('/login', function (req, res) {
   });
 });
 
-// user submits the signup form
+// create new user with secure password
 app.post('/users', function (req, res) {
-
-  // grab user data from params (req.body)
   var newUser = req.body.user;
-
-  // create new user with secure password
-  User.createSecure(newUser.email, newUser.password, function (err, user) {
-    res.redirect('/');
+  User.createSecure(newUser, function (err, user) {
+    console.log("You have entered the /users function!");
+    // log in user immediately when created
+    req.login(user); //this does not seem to be working
+    res.redirect('/profile');
   });
 });
 
@@ -114,7 +120,50 @@ app.get('/logout', function (req, res) {
 // create an api/profile route so users can access their information from elsewhere via an api, as long as they're logged in
 
 
-// listen on port 3000
-app.listen(3000, function(){
-  console.log('Server started on localhost:3000');
+// API Routes
+
+// show current user
+app.get('/api/users/current', function (req, res) { // returns all info on a current user; question: is creating a public api like this secure? the user's hashed and salted password is available to anyone with the api URL, or is this not a concern because the user has to be logged in for the api to be available?
+  // check for current (logged-in) user
+  req.currentUser(function (err, user) {
+    console.log(user);
+    res.json(user);
+  });
 });
+
+// show all logs
+app.get('/api/logs', function (req, res) {
+  Log.find(function (err, logs) {
+    res.json(logs);
+  });
+});
+
+// create new log
+app.post('/api/logs', function (req, res) {
+
+  var logData = req.body.log;
+
+  var currentdate = new Date();
+  var datetime = (currentdate.getMonth()+1) + "/"
+                  + currentdate.getDate() + "/"
+                  + currentdate.getFullYear() + " @ "
+                  + currentdate.getHours() + ":"
+                  + currentdate.getMinutes() + ":"
+                  + currentdate.getSeconds();
+
+  // create new instance of Log
+  var newLog = new Log({
+    diary_entry: logData.diary_entry,
+    date: datetime,
+    user: req.currentUser.id
+  });
+
+  // save new log in db
+newLog.save(function (err, savedLog) {
+  res.json(savedLog);
+});
+});
+
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000);
